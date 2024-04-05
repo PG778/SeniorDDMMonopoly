6d1385ea2d390da34b65690ac35c99f2e6b7c531
PG778 <castillolb@oldcolony.info>
2024-04-05 14:05:07 -0400

    Working On Making Sure Database Items Work

--- src/main/java/com/example/monopoly_lc_ddm/Game.java
+++ src/main/java/com/example/monopoly_lc_ddm/Game.java
@@ -17,4 +17,94 @@ public class Game {
     public int getId() {
         return id;
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void startGame() {
+    }
+
+    public void endGame() {
+    }
+
+    public void restartGame() {
+    }
+
+    public void addPlayer() {
+    }
+
+    public void removePlayer() {
+    }
+
+    public void updatePlayer() {
+    }
+
+    public void updateBoard() {
+    }
+
+    public void updateBank() {
+    }
+
+    public void updateProperty() {
+    }
+
+    public void updateCommunityChest() {
+    }
+
+    public void updateChance() {
+    }
+
+    public void updateJail() {
+    }
+
+    public void updateFreeParking() {
+    }
+
+    public void updateGo() {
+    }
+
+    public void updateGoToJail() {
+    }
+
+    public void updateIncomeTax() {
+    }
+
+    public void updateLuxuryTax() {
+    }
+
+    public void updateRailroad() {
+    }
+
+    public void updateUtility() {
+    }
+
+    public void updateChanceCard() {
+    }
+
+    public void updateCommunityChestCard() {
+    }
+
+    public void updatePropertyCard() {
+    }
+
+    public void updateRailroadCard() {
+    }
+
+    public void updateUtilityCard() {
+    }
+
+    public void updatePlayerCard() {
+    }
+
+    public void updateBankCard() {
+    }
+
+   
+
+
 }
--- src/main/java/com/example/monopoly_lc_ddm/Player.java
+++ src/main/java/com/example/monopoly_lc_ddm/Player.java
@@ -7,6 +7,15 @@ public class Player {
     private int gameId;
     private int balance;
 
+    private boolean hasColorSet;
+
+    private int hotelPrice = 100;
+
+    private boolean isInJail;
+
+    private int jailTurns = 3;
+
+    private boolean hasRolledDoubles;
 
 
     public Player(int id, int gameId, int balance) {
@@ -29,5 +38,62 @@ public class Player {
         return id;
     }
 
+
+    public int AddBalance(int amount){
+        balance += amount;
+        return balance;
+    }
+    public int DeductBalance(int amount){
+        balance -= amount;
+        return balance;
+    }
+
+    public int buyProperty(int amount){
+        balance -= amount;
+        return balance;
+    }
+    public int sellProperty(int amount){
+        balance += amount;
+        return balance;
+    }
+    public void updateBoardPosition(int position){
+
+    }
+
+    public boolean getHasColorSet (){
+        return hasColorSet;
+    }
+
+    public boolean setHasColorSet(boolean hasColorSet){
+        // If the player has a complete color set (Setting Up later in project) then return true, if not return false
+        return hasColorSet;
+    }
+
+    public boolean getIsInJail(){
+        return isInJail;
+    }
+
+
+
+
+
+    private int hasRolledDoubles(){
+
+        return 0;
+    }
+
+    public boolean setIsInJail(boolean isInJail){
+        // If the player is in jail, return true, if not return false
+        if(isInJail){
+            if(hasRolledDoubles() == 1){
+                isInJail = false;
+                return isInJail;
+            }
+            else{
+                isInJail = true;
+                return isInJail;
+            }
+        }
+    }
 
 }
